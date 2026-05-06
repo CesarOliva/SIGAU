06b168f202da61eca1840585a993899fcf83326d
Cesar Oliva <cesaroliva.work@gmail.com>
2026-05-05 19:05:59 -0600

    empty page at gestionar-materias

---
+++ app/(app)/administrador/gestionar-materias/page.tsx
@@ -0,0 +1,9 @@
+const MateriasPage = () => {
+    return (
+        <div className="p-6">
+            <h1 className="text-2xl font-bold">Gestionar materias</h1>
+        </div>
+    );
+}
+ 
+export default MateriasPage;
